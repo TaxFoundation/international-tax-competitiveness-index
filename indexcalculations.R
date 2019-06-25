28c5253381ab623838ef99afddac78adef3469fe
Daniel Bunn <dbunn@taxfoundation.org>
2019-06-25 14:42:17 -0400

    R Code Update 3

--- indexcalculations.R
+++ indexcalculations.R
@@ -1,8 +1,32 @@
-# Sets the working directory. This sets it to the "index" folder on my desktop
-library(plyr)
-library(dplyr)
+using<-function(...,prompt=TRUE){
+  libs<-sapply(substitute(list(...))[-1],deparse)
+  req<-unlist(lapply(libs,require,character.only=TRUE))
+  need<-libs[req==FALSE]
+  n<-length(need)
+  installAndRequire<-function(){
+    install.packages(need)
+    lapply(need,require,character.only=TRUE)
+  }
+  if(n>0){
+    libsmsg<-if(n>2) paste(paste(need[1:(n-1)],collapse=", "),",",sep="") else need[1]
+    if(n>1){
+      libsmsg<-paste(libsmsg," and ", need[n],sep="")
+    }
+    libsmsg<-paste("The following packages count not be found: ",libsmsg,"n\r\n\rInstall missing packages?",collapse="")
+    if(prompt==FALSE){
+      installAndRequire()
+    }else if(winDialog(type=c("yesno"),libsmsg)=="YES"){
+      installAndRequire()
+    }
+  }
+}
 
-setwd("C:/Users/kep/Dropbox (Personal)/Work/international-tax-competitiveness-index/2018 Index")
+# Sets the working directory. This sets it to the "index" folder on my desktop
+using(plyr)
+using(dplyr)
+using(tidyverse)
+
+setwd("C:/Users/dbunn/Dropbox (Tax Foundation)/international-tax-competitiveness-index/2018 Index")
 
 #Clears all datasets and variables from memory
 
@@ -12,7 +36,7 @@ rm(list=ls())
 rawdata2014<-read.csv("indexdata2014.csv", header = TRUE, fill = TRUE, sep = ",")
 rawdata2014$year<-2014
 #2015
-rawdata2015<-read.csv("indexdata2015.csv", header = TRUE, fill = TRUE, sep = ",")
+rawdata2015<-read.csv("indexdata2015db.csv", header = TRUE, fill = TRUE, sep = ",")
   #rawdata2015<-rename(rawdata2015, c("country.limitations"="countrylimitations"))
   rawdata2015$year<-2015
 #2016
@@ -484,4 +508,4 @@ check<-rawdata[rawdata$country == "Greece",]
 
       cortest2<-ALTfinalcategories[ALTfinalcategories$year == 2015,]
       cor(cortest2[c(4,6,8,10,12,14)])     
-      write.csv(Final2017, file = "data2017run.csv")
+      write.csv(Final2018, file = "data2018run.csv")
