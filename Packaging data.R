752a8b11606bf62fb922b5bd748c17c6b1b68d28
Daniel Bunn <dbunn@taxfoundation.org>
2019-07-10 16:54:06 -0400

    Add read-ins for csvs

---
+++ Packaging data.R
@@ -0,0 +1,36 @@
+#Packaging Data
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Clears all datasets and variables from memory
+rm(list=ls())
+
+using<-function(...,prompt=TRUE){
+  libs<-sapply(substitute(list(...))[-1],deparse)
+  req<-unlist(lapply(libs,require,character.only=TRUE))
+  need<-libs[req==FALSE]
+  n<-length(need)
+  installAndRequire<-function(){
+    install.packages(need)
+    lapply(need,require,character.only=TRUE)
+  }
+  if(n>0){
+    libsmsg<-if(n>2) paste(paste(need[1:(n-1)],collapse=", "),",",sep="") else need[1]
+    if(n>1){
+      libsmsg<-paste(libsmsg," and ", need[n],sep="")
+    }
+    libsmsg<-paste("The following packages count not be found: ",libsmsg,"n\r\n\rInstall missing packages?",collapse="")
+    if(prompt==FALSE){
+      installAndRequire()
+    }else if(winDialog(type=c("yesno"),libsmsg)=="YES"){
+      installAndRequire()
+    }
+  }
+}
+
+using(readr)
+using(tidyverse)
+
+CFC_Rules<-read_csv("CFC Rules Data.csv")
+Cap_Allowances<-read_csv("cap_allowances_data.csv")
+OECD_vars<-read_csv("OECDvars_data.csv")
