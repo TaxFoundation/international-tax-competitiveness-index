a5072314d3ec56245634e04bad474225beb89a2d
Daniel Bunn <dbunn@taxfoundation.org>
2019-07-18 09:27:04 -0400

    Initial VAT Data code

--- VAT Data.R
+++ VAT Data.R
@@ -54,14 +54,53 @@ colnames(vat_data)<-c("country","year","vatrate")
 vat_data$country <- str_remove_all(vat_data$country, "[*]")
 
 
-write.csv(vat_data,"vatrates.csv",row.names = FALSE)
+#write.csv(vat_data,"vatrates.csv",row.names = FALSE)
 
 #VAT Thresholds
-vat_thresholds <- read_excel("source-data/vat-gst-annual-turnover-concessions-ctt-trends.xlsx", range = "A4:e42")
+vat_thresholds_2018 <- read_excel("source-data/vat-gst-annual-turnover-concessions-ctt-trends.xlsx", sheet = "2018", range = "A4:e42")
+vat_thresholds_2016 <- read_excel("source-data/vat-gst-annual-turnover-concessions-ctt-trends.xlsx", sheet = "2016", range = "a4:e41")
+vat_thresholds_2014 <- read_excel("source-data/vat-gst-annual-turnover-concessions-ctt-trends.xlsx", sheet = "2014", range = "a4:d37")
 
-vat_thresholds<-vat_thresholds[-c(2:4)]
-colnames(vat_thresholds)<-c("country","threshold")
 
-vat_thresholds$country <- stringr::str_replace(vat_thresholds$country, '\\*', '')
-vat_thresholds$country <- str_remove_all(vat_thresholds$country, "[6*]")
+
+vat_thresholds_2018<-vat_thresholds_2018[-c(2:4)]
+colnames(vat_thresholds_2018)<-c("country","threshold")
+vat_thresholds_2018$country <- str_remove_all(vat_thresholds_2018$country, "[6*]")
+vat_thresholds_2018$year<-"2018"
+vat_thresholds_2019<-vat_thresholds_2018
+vat_thresholds_2019$year<-"2019"
+
+vat_thresholds_2016<-vat_thresholds_2016[-c(2:4)]
+colnames(vat_thresholds_2016)<-c("country","threshold")
+vat_thresholds_2016$country <- str_remove_all(vat_thresholds_2016$country, "[(f)*]")
+vat_thresholds_2016$year<-"2016"
+vat_thresholds_2017<-vat_thresholds_2016
+vat_thresholds_2017$year<-"2017"
+
+vat_thresholds_2014<-vat_thresholds_2014[-c(2:3)]
+colnames(vat_thresholds_2014)<-c("country","threshold")
+vat_thresholds_2014$country <- str_remove_all(vat_thresholds_2014$country, "[*]")
+vat_thresholds_2014$year<-"2014"
+vat_thresholds_2015<-vat_thresholds_2014
+vat_thresholds_2015$year<-"2015"
+
+vat_thresholds<-rbind(vat_thresholds_2014,vat_thresholds_2015,vat_thresholds_2016,vat_thresholds_2017,vat_thresholds_2018,vat_thresholds_2019)
+
+#Change NAs to zeros
+vat_thresholds[is.na(vat_thresholds)] <- 0
+
+
+#Add US and other countries
+columns<-names(vat_thresholds)
+US_2014<-c("United States","0","2014")
+US_2015<-c("United States","0","2015")
+US_2016<-c("United States","0","2016")
+US_2017<-c("United States","0","2017")
+US_2018<-c("United States","0","2018")
+US_2019<-c("United States","0","2019")
+
+
+
+additional_countries <- data.frame(columns, US_2014, US_2015, US_2016,US_2017,US_2018,US_2019)
+US<-gather(columns,values)
 
