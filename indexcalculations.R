9758dd1b74caf8724842df8f134708046d9335d9
Daniel Bunn <dbunn@taxfoundation.org>
2019-08-05 12:45:45 -0400

    Second run for 2019

--- indexcalculations.R
+++ indexcalculations.R
@@ -56,6 +56,8 @@ rawdata2019 <- read_csv("./final-data/final_indexdata2019.csv")
 #Combined Data
 rawdata<-rbind(rawdata2014,rawdata2015,rawdata2016,rawdata2017,rawdata2018,rawdata2019)
 
+write.csv(rawdata,"./final-outputs/Raw Data 2019.csv")
+
 rawdata$patentbox<-as.numeric(rawdata$patentbox)
 rawdata$rndcredit<-as.numeric(rawdata$rndcredit)
 rawdata$netwealth<-as.numeric(rawdata$netwealth)
