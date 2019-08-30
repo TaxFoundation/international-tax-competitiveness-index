f829b27760e130b65ca73b8ad72fe39501b7bd49
Daniel Bunn <dbunn@taxfoundation.org>
2019-08-30 13:53:48 -0400

    OECD data ISO variables

--- OECD data scraper.R
+++ OECD data scraper.R
@@ -213,9 +213,9 @@ OECDvars_data<-merge(OECDvars_data,tax_wedge,by=c("Country","Year"))
 #Load ISO Country Codes####
 #Source: https://www.cia.gov/library/publications/the-world-factbook/appendix/appendix-d.html
 ISO_Country_Codes <- read_csv("./source-data/ISO Country Codes.csv")
-colnames(ISO_Country_Codes)<-c("country","ISO-2","ISO-3")
+colnames(ISO_Country_Codes)<-c("country","ISO_2","ISO_3")
 
-colnames(OECDvars_data)<-c("ISO-3","year","corporate_rate","dividends_rate", "top_income_rate", "threshold_top_income_rate", "tax_wedge")
-OECDvars_data<-merge(OECDvars_data,ISO_Country_Codes,by="ISO-3")
+colnames(OECDvars_data)<-c("ISO_3","year","corporate_rate","dividends_rate", "top_income_rate", "threshold_top_income_rate", "tax_wedge")
+OECDvars_data<-merge(OECDvars_data,ISO_Country_Codes,by="ISO_3")
 
 write.csv(OECDvars_data, file = "./intermediate-outputs/OECDvars_data.csv", row.names = FALSE)
