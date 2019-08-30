12072b266ce2c259d8ad7fb7c27dfc83993179bc
Daniel Bunn <dbunn@taxfoundation.org>
2019-08-30 13:09:48 -0400

    small additional change

--- reordering_columns.R
+++ reordering_columns.R
@@ -38,14 +38,14 @@ using(scales)
 #Load ISO Country Codes####
 #Source: https://www.cia.gov/library/publications/the-world-factbook/appendix/appendix-d.html
 ISO_Country_Codes <- read_csv("./source-data/ISO Country Codes.csv")
-colnames(ISO_Country_Codes)<-c("country","ISO-2","ISO-3")
+colnames(ISO_Country_Codes)<-c("country","ISO_2","ISO_3")
 
 #Reordering columns for source data####
 
 indexdata2014<-read_csv("./source-data/indexdata2014.csv")
 
 indexdata2014<-merge(indexdata2014,ISO_Country_Codes,by=c("country"))
-indexdata2014<-indexdata2014[c("ISO-2","ISO-3","country",
+indexdata2014<-indexdata2014[c("ISO_2","ISO_3","country",
                                "corporate_rate","loss_carryback","loss_carryforward","machines_cost_recovery","buildings_cost_recovery","intangibles_cost_recovery","inventory","patent_box","r_and_d_credit","corporate_time","profit_payments","other_payments",
                                "top_income_rate","threshold_top_income_rate","tax_wedge","labor_payments","labor_time","capital_gains_rate","index_capital_gains","dividends_rate",
                                "vat_rate","threshold_vat","base_vat","consumption_time",
@@ -56,7 +56,7 @@ write.csv(indexdata2014,"./source-data/indexdata2014.csv",row.names = F)
 
 indexdata2015<-read_csv("./source-data/indexdata2015.csv")
 indexdata2015<-merge(indexdata2015,ISO_Country_Codes,by=c("country"))
-indexdata2015<-indexdata2015[c("ISO-2","ISO-3","country",
+indexdata2015<-indexdata2015[c("ISO_2","ISO_3","country",
                                "corporate_rate","loss_carryback","loss_carryforward","machines_cost_recovery","buildings_cost_recovery","intangibles_cost_recovery","inventory","patent_box","r_and_d_credit","corporate_time","profit_payments","other_payments",
                                "top_income_rate","threshold_top_income_rate","tax_wedge","labor_payments","labor_time","capital_gains_rate","index_capital_gains","dividends_rate",
                                "vat_rate","threshold_vat","base_vat","consumption_time",
@@ -67,7 +67,7 @@ write.csv(indexdata2015,"./source-data/indexdata2015.csv",row.names = F)
 
 indexdata2016<-read_csv("./source-data/indexdata2016.csv")
 indexdata2016<-merge(indexdata2016,ISO_Country_Codes,by=c("country"))
-indexdata2016<-indexdata2016[c("ISO-2","ISO-3","country",
+indexdata2016<-indexdata2016[c("ISO_2","ISO_3","country",
                                "corporate_rate","loss_carryback","loss_carryforward","machines_cost_recovery","buildings_cost_recovery","intangibles_cost_recovery","inventory","patent_box","r_and_d_credit","corporate_time","profit_payments","other_payments",
                                "top_income_rate","threshold_top_income_rate","tax_wedge","labor_payments","labor_time","capital_gains_rate","index_capital_gains","dividends_rate",
                                "vat_rate","threshold_vat","base_vat","consumption_time",
@@ -79,7 +79,7 @@ write.csv(indexdata2016,"./source-data/indexdata2016.csv",row.names = F)
 
 indexdata2017<-read_csv("./source-data/indexdata2017.csv")
 indexdata2017<-merge(indexdata2017,ISO_Country_Codes,by=c("country"))
-indexdata2017<-indexdata2017[c("ISO-2","ISO-3","country",
+indexdata2017<-indexdata2017[c("ISO_2","ISO_3","country",
                                "corporate_rate","loss_carryback","loss_carryforward","machines_cost_recovery","buildings_cost_recovery","intangibles_cost_recovery","inventory","patent_box","r_and_d_credit","corporate_time","profit_payments","other_payments",
                                "top_income_rate","threshold_top_income_rate","tax_wedge","labor_payments","labor_time","capital_gains_rate","index_capital_gains","dividends_rate",
                                "vat_rate","threshold_vat","base_vat","consumption_time",
@@ -90,7 +90,7 @@ write.csv(indexdata2017,"./source-data/indexdata2017.csv",row.names = F)
 
 indexdata2018<-read_csv("./source-data/indexdata2018.csv")
 indexdata2018<-merge(indexdata2018,ISO_Country_Codes,by=c("country"))
-indexdata2018<-indexdata2018[c("ISO-2","ISO-3","country",
+indexdata2018<-indexdata2018[c("ISO_2","ISO_3","country",
                                "corporate_rate","loss_carryback","loss_carryforward","machines_cost_recovery","buildings_cost_recovery","intangibles_cost_recovery","inventory","patent_box","r_and_d_credit","corporate_time","profit_payments","other_payments",
                                "top_income_rate","threshold_top_income_rate","tax_wedge","labor_payments","labor_time","capital_gains_rate","index_capital_gains","dividends_rate",
                                "vat_rate","threshold_vat","base_vat","consumption_time",
@@ -101,7 +101,7 @@ write.csv(indexdata2018,"./source-data/indexdata2018.csv",row.names = F)
 
 indexdata2019<-read_csv("./source-data/indexdata2019.csv")
 indexdata2019<-merge(indexdata2019,ISO_Country_Codes,by=c("country"))
-indexdata2019<-indexdata2019[c("ISO-2","ISO-3","country",
+indexdata2019<-indexdata2019[c("ISO_2","ISO_3","country",
                                "corporate_rate","loss_carryback","loss_carryforward","machines_cost_recovery","buildings_cost_recovery","intangibles_cost_recovery","inventory","patent_box","r_and_d_credit","corporate_time","profit_payments","other_payments",
                                "top_income_rate","threshold_top_income_rate","tax_wedge","labor_payments","labor_time","capital_gains_rate","index_capital_gains","dividends_rate",
                                "vat_rate","threshold_vat","base_vat","consumption_time",
