751d5d4971473eae313ca88b54e3866b5da9bc7b
Daniel Bunn <dbunn@taxfoundation.org>
2019-09-12 10:01:44 -0400

    rename directories

---
+++ R_code_files/00_master_file.R
@@ -0,0 +1,45 @@
+#Clean up working environment
+rm(list=ls())
+gc()
+
+#Directory Variables
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+R_code_files<-"C:/Github/international-tax-competitiveness-index/R_code_files"
+source_data<-"C:/Github/international-tax-competitiveness-index/source_data"
+intermediate_outputs<-"C:/Github/international-tax-competitiveness-index/intermediate_outputs"
+final_outputs<-"C:/Github/international-tax-competitiveness-index/final_outputs"
+
+
+#Define Using function
+using<-function(...,prompt=TRUE){
+  libs<-sapply(substitute(list(...))[-1],deparse)
+  req<-unlist(lapply(libs,require,character.only=TRUE))
+  need<-libs[req==FALSE]
+  n<-length(need)
+  installAndRequire<-function(){
+    install.packages(need)
+    lapply(need,require,character.only=TRUE)
+  }
+  if(n>0){
+    libsmsg<-if(n>2) paste(paste(need[1:(n-1)],collapse=", "),",",sep="") else need[1]
+    if(n>1){
+      libsmsg<-paste(libsmsg," and ", need[n],sep="")
+    }
+    libsmsg<-paste("The following packages count not be found: ",libsmsg,"n\r\n\rInstall missing packages?",collapse="")
+    if(prompt==FALSE){
+      installAndRequire()
+    }else if(winDialog(type=c("yesno"),libsmsg)=="YES"){
+      installAndRequire()
+    }
+  }
+}
+#Run code files
+source("01_oecd_data_scraper.R")
+source("02_cost_recovery.R")
+source("03_vat_data.R")
+source("04_property_tax_collections.R")
+source("05_cfc_rules.R")
+source("06_packaging_data.R")
+source("07_index_calculations.R")
+source("08_output_tables.R")
+source("09_country_profiles.R")
--- R_code_files/01_oecd_data_scraper.R
+++ R_code_files/01_oecd_data_scraper.R
@@ -1,33 +1,4 @@
 #OECD data scraper
-
-rm(list=ls())
-gc()
-
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
-using<-function(...,prompt=TRUE){
-  libs<-sapply(substitute(list(...))[-1],deparse)
-  req<-unlist(lapply(libs,require,character.only=TRUE))
-  need<-libs[req==FALSE]
-  n<-length(need)
-  installAndRequire<-function(){
-    install.packages(need)
-    lapply(need,require,character.only=TRUE)
-  }
-  if(n>0){
-    libsmsg<-if(n>2) paste(paste(need[1:(n-1)],collapse=", "),",",sep="") else need[1]
-    if(n>1){
-      libsmsg<-paste(libsmsg," and ", need[n],sep="")
-    }
-    libsmsg<-paste("The following packages count not be found: ",libsmsg,"n\r\n\rInstall missing packages?",collapse="")
-    if(prompt==FALSE){
-      installAndRequire()
-    }else if(winDialog(type=c("yesno"),libsmsg)=="YES"){
-      installAndRequire()
-    }
-  }
-}
-
 using(OECD)
 using(tidyverse)
 using(readxl)
