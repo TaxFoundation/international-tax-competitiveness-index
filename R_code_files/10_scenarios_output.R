c13f6feb3e8c9408de42aa397aeb952a98ca5ba1
Daniel Bunn <dbunn@taxfoundation.org>
2020-04-10 08:49:25 -0400

    Final step UK scenarios

--- R_code_files/10_scenarios_output.R
+++ R_code_files/10_scenarios_output.R
@@ -1,11 +1,13 @@
 baseline <- read_csv(paste(final_outputs,"United_Kingdom_Baseline.csv",sep=""))
 Option_A <- read_csv(paste(final_outputs,"United_Kingdom_Option_A.csv",sep=""))
 Option_B <- read_csv(paste(final_outputs,"United_Kingdom_Option_B.csv",sep=""))
+Budget_2020 <- read_csv(paste(final_outputs,"United_Kingdom_Budget_2020.csv",sep=""))
 baseline$scenario<-"Baseline"
 Option_A$scenario<-"Option A"
 Option_B$scenario<-"Option B"
+Budget_2020$scenario<-"Budget 2020"
 
-scenarios<-rbind(baseline,Option_A,Option_B)
+scenarios<-rbind(baseline,Option_A,Option_B,Budget_2020)
 scenarios<-subset(scenarios,scenarios$year==2019)
 scenarios<-scenarios[-c(1:2,4,6,8,10,12,14:16)]
 names(scenarios)
