8aff957aef17ad009488a47e09f67073659a780a
Daniel Bunn <dbunn@taxfoundation.org>
2020-06-08 16:02:01 -0400

    Paying taxes data R code and transposed output
    Take PwC's spreadsheet and turn it into a useful .csv file.

---
+++ R_code_files/paying_taxes.R
@@ -0,0 +1,174 @@
+#PWC Paying Taxes Data####
+
+#Import data
+index_data2014<-read_csv(paste(source_data,"index_data_2014.csv",sep=""))
+index_data2014$year<-2014
+
+pwc_paying_taxes <- read_excel(paste(source_data,"pwc_paying_taxes.xlsx",sep=""),
+                               range = "B1:AR191")
+
+#Rename variables
+pwc_paying_taxes<-
+  pwc_paying_taxes %>%
+  rename(
+     country = X__1,
+     region = X__2,
+     income = X__3,
+     corporate_time_2018 = `2018`,
+     labor_time_2018 = `2018__1`,
+     consumption_time_2018 = `2018__2`,
+     total_time_2018 = `2018__3`,
+     profit_payments_2018 = `2018__4`,
+     labor_payments_2018 = `2018__5`,
+     consumption_payments_2018 = `2018__6`,
+     total_payments_2018 = `2018__7`,
+     corporate_time_2017 = `2017`,
+     labor_time_2017 = `2017__1`,
+     consumption_time_2017 = `2017__2`,
+     total_time_2017 = `2017__3`,
+     profit_payments_2017 = `2017__4`,
+     labor_payments_2017 = `2017__5`,
+     consumption_payments_2017 = `2017__6`,
+     total_payments_2017 = `2017__7`,
+     corporate_time_2016 = `2016`,
+     labor_time_2016 = `2016__1`,
+     consumption_time_2016 = `2016__2`,
+     total_time_2016 = `2016__3`,
+     profit_payments_2016 = `2016__4`,
+     labor_payments_2016 = `2016__5`,
+     consumption_payments_2016 = `2016__6`,
+     total_payments_2016 = `2016__7`,
+     corporate_time_2015 = `2015`,
+     labor_time_2015 = `2015__1`,
+     consumption_time_2015 = `2015__2`,
+     total_time_2015 = `2015__3`,
+     profit_payments_2015 = `2015__4`,
+     labor_payments_2015 = `2015__5`,
+     consumption_payments_2015 = `2015__6`,
+     total_payments_2015 = `2015__7`,
+     corporate_time_2014 = `2014`,
+     labor_time_2014 = `2014__1`,
+     consumption_time_2014 = `2014__2`,
+     total_time_2014 = `2014__3`,
+     profit_payments_2014 = `2014__4`,
+     labor_payments_2014 = `2014__5`,
+     consumption_payments_2014 = `2014__6`,
+     total_payments_2014 = `2014__7`
+  )
+
+#Drop redundant row
+pwc_paying_taxes <- pwc_paying_taxes[-c(1), ]
+
+#Join with country names
+pwc_paying_taxes<-merge(pwc_paying_taxes,iso_country_codes,by=c("country"),all=T)
+
+#Subset for OECD countries
+pwc_paying_taxes<-subset(pwc_paying_taxes,pwc_paying_taxes$ISO_3 %in% oecd_countries)
+
+#Transpose data year by year####
+#2018####
+pwc_paying_taxes_2018<-pwc_paying_taxes %>%
+  select(country:total_payments_2018,ISO_2,ISO_3)%>%
+  gather(variable,value,corporate_time_2018:total_payments_2018)
+pwc_paying_taxes_2018$year<-2018
+
+pwc_paying_taxes_2018<-pwc_paying_taxes_2018%>%
+    spread(variable,value)
+
+pwc_paying_taxes_2018<-  pwc_paying_taxes_2018 %>%
+  rename(corporate_time = corporate_time_2018,
+    labor_time = labor_time_2018,
+    consumption_time = consumption_time_2018,
+    total_time = total_time_2018,
+    profit_payments = profit_payments_2018,
+    labor_payments = labor_payments_2018,
+    consumption_payments = consumption_payments_2018,
+    total_payments = total_payments_2018)
+
+#2017####
+pwc_paying_taxes_2017<-pwc_paying_taxes %>%
+  select(country:income, corporate_time_2017:total_payments_2017,ISO_2,ISO_3)%>%
+  gather(variable,value,corporate_time_2017:total_payments_2017)
+pwc_paying_taxes_2017$year<-2017
+
+pwc_paying_taxes_2017<-pwc_paying_taxes_2017%>%
+  spread(variable,value)
+
+pwc_paying_taxes_2017<-  pwc_paying_taxes_2017 %>%
+  rename(corporate_time = corporate_time_2017,
+         labor_time = labor_time_2017,
+         consumption_time = consumption_time_2017,
+         total_time = total_time_2017,
+         profit_payments = profit_payments_2017,
+         labor_payments = labor_payments_2017,
+         consumption_payments = consumption_payments_2017,
+         total_payments = total_payments_2017)
+
+#2016####
+pwc_paying_taxes_2016<-pwc_paying_taxes %>%
+  select(country:income, corporate_time_2016:total_payments_2016,ISO_2,ISO_3)%>%
+  gather(variable,value,corporate_time_2016:total_payments_2016)
+pwc_paying_taxes_2016$year<-2016
+
+pwc_paying_taxes_2016<-pwc_paying_taxes_2016%>%
+  spread(variable,value)
+
+pwc_paying_taxes_2016<-  pwc_paying_taxes_2016 %>%
+  rename(corporate_time = corporate_time_2016,
+         labor_time = labor_time_2016,
+         consumption_time = consumption_time_2016,
+         total_time = total_time_2016,
+         profit_payments = profit_payments_2016,
+         labor_payments = labor_payments_2016,
+         consumption_payments = consumption_payments_2016,
+         total_payments = total_payments_2016)
+
+#2015####
+pwc_paying_taxes_2015<-pwc_paying_taxes %>%
+  select(country:income,corporate_time_2015:total_payments_2015,ISO_2,ISO_3)%>%
+  gather(variable,value,corporate_time_2015:total_payments_2015)
+pwc_paying_taxes_2015$year<-2015
+
+pwc_paying_taxes_2015<-pwc_paying_taxes_2015%>%
+  spread(variable,value)
+
+pwc_paying_taxes_2015<-  pwc_paying_taxes_2015 %>%
+  rename(corporate_time = corporate_time_2015,
+         labor_time = labor_time_2015,
+         consumption_time = consumption_time_2015,
+         total_time = total_time_2015,
+         profit_payments = profit_payments_2015,
+         labor_payments = labor_payments_2015,
+         consumption_payments = consumption_payments_2015,
+         total_payments = total_payments_2015)
+
+#2014####
+pwc_paying_taxes_2014<-pwc_paying_taxes %>%
+  select(country:income,corporate_time_2014:total_payments_2014,ISO_2,ISO_3)%>%
+  gather(variable,value,corporate_time_2014:total_payments_2014)
+pwc_paying_taxes_2014$year<-2014
+
+pwc_paying_taxes_2014<-pwc_paying_taxes_2014%>%
+  spread(variable,value)
+
+pwc_paying_taxes_2014<-  pwc_paying_taxes_2014 %>%
+  rename(corporate_time = corporate_time_2014,
+         labor_time = labor_time_2014,
+         consumption_time = consumption_time_2014,
+         total_time = total_time_2014,
+         profit_payments = profit_payments_2014,
+         labor_payments = labor_payments_2014,
+         consumption_payments = consumption_payments_2014,
+         total_payments = total_payments_2014)
+
+pwc_paying_taxes<-rbind(pwc_paying_taxes_2014,
+                          pwc_paying_taxes_2015,
+                          pwc_paying_taxes_2016,
+                          pwc_paying_taxes_2017,
+                          pwc_paying_taxes_2018)
+
+pwc_paying_taxes<-pwc_paying_taxes[,-c(2:3,11:12)]
+
+
+write.csv(pwc_paying_taxes, file = paste(intermediate_outputs,"pwc_paying_taxes.csv",sep=""), 
+          row.names = FALSE)
