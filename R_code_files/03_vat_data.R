a5e46edc5dc0e452017becd976b40b34202838c0
Daniel Bunn <dbunn@taxfoundation.org>
2020-07-07 11:45:21 -0400

    clean up the R code

--- R_code_files/03_vat_data.R
+++ R_code_files/03_vat_data.R
@@ -35,27 +35,20 @@ write.csv(vat_rates,paste(intermediate_outputs,"vat_rates.csv",sep=""),row.names
 
 
 #VAT Thresholds####
-
-vat_thresholds_2020 <- read_excel(paste(source_data,"oecd_vat_gst_annual_turnover_concessions_ctt_trends.xlsx",sep=""), sheet = "2020", range = "A4:e42")
-vat_thresholds_2018 <- read_excel(paste(source_data,"oecd_vat_gst_annual_turnover_concessions_ctt_trends.xlsx",sep=""), sheet = "2018", range = "A4:e42")
-vat_thresholds_2016 <- read_excel(paste(source_data,"oecd_vat_gst_annual_turnover_concessions_ctt_trends.xlsx",sep=""), sheet = "2016", range = "a4:e41")
 vat_thresholds_2014 <- read_excel(paste(source_data,"oecd_vat_gst_annual_turnover_concessions_ctt_trends.xlsx",sep=""), sheet = "2014", range = "a4:d37")
+vat_thresholds_2016 <- read_excel(paste(source_data,"oecd_vat_gst_annual_turnover_concessions_ctt_trends.xlsx",sep=""), sheet = "2016", range = "a4:e41")
+vat_thresholds_2018 <- read_excel(paste(source_data,"oecd_vat_gst_annual_turnover_concessions_ctt_trends.xlsx",sep=""), sheet = "2018", range = "A4:e42")
+vat_thresholds_2020 <- read_excel(paste(source_data,"oecd_vat_gst_annual_turnover_concessions_ctt_trends.xlsx",sep=""), sheet = "2020", range = "A4:e42")
 
-#2020
-vat_thresholds_2020 <- vat_thresholds_2020[-c(2:4)]
-colnames(vat_thresholds_2020) <- c("country", "vat_threshold")
-vat_thresholds_2020$country <- str_remove_all(vat_thresholds_2020$country, "[6*]")
-vat_thresholds_2020$year <- "2020"
+#2014
+vat_thresholds_2014 <- vat_thresholds_2014[-c(2:3)]
+colnames(vat_thresholds_2014) <- c("country","vat_threshold")
+vat_thresholds_2014$country <- str_remove_all(vat_thresholds_2014$country, "[*]")
+vat_thresholds_2014$year <- "2014"
 
-#2018
-vat_thresholds_2018 <- vat_thresholds_2018[-c(2:4)]
-colnames(vat_thresholds_2018) <- c("country", "vat_threshold")
-vat_thresholds_2018$country <- str_remove_all(vat_thresholds_2018$country, "[6*]")
-vat_thresholds_2018$year <- "2018"
-
-#2019
-vat_thresholds_2019 <- vat_thresholds_2018
-vat_thresholds_2019$year <- "2019"
+#2015
+vat_thresholds_2015 <- vat_thresholds_2014
+vat_thresholds_2015$year <- "2015"
 
 #2016
 vat_thresholds_2016 <- vat_thresholds_2016[-c(2:4)]
@@ -67,15 +60,21 @@ vat_thresholds_2016$year <- "2016"
 vat_thresholds_2017 <- vat_thresholds_2016
 vat_thresholds_2017$year <- "2017"
 
-#2014
-vat_thresholds_2014 <- vat_thresholds_2014[-c(2:3)]
-colnames(vat_thresholds_2014) <- c("country","vat_threshold")
-vat_thresholds_2014$country <- str_remove_all(vat_thresholds_2014$country, "[*]")
-vat_thresholds_2014$year <- "2014"
+#2018
+vat_thresholds_2018 <- vat_thresholds_2018[-c(2:4)]
+colnames(vat_thresholds_2018) <- c("country", "vat_threshold")
+vat_thresholds_2018$country <- str_remove_all(vat_thresholds_2018$country, "[6*]")
+vat_thresholds_2018$year <- "2018"
 
-#2015
-vat_thresholds_2015 <- vat_thresholds_2014
-vat_thresholds_2015$year <- "2015"
+#2019
+vat_thresholds_2019 <- vat_thresholds_2018
+vat_thresholds_2019$year <- "2019"
+
+#2020
+vat_thresholds_2020 <- vat_thresholds_2020[-c(2:4)]
+colnames(vat_thresholds_2020) <- c("country", "vat_threshold")
+vat_thresholds_2020$country <- str_remove_all(vat_thresholds_2020$country, "[6*]")
+vat_thresholds_2020$year <- "2020"
 
 #Combine years
 vat_thresholds <- rbind(vat_thresholds_2014, vat_thresholds_2015, vat_thresholds_2016, vat_thresholds_2017, vat_thresholds_2018, vat_thresholds_2019, vat_thresholds_2020)
