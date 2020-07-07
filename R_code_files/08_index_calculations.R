3d75fdff588501fcc157bf13c57f7f13cff4e032
Daniel Bunn <dbunn@taxfoundation.org>
2020-07-07 14:48:18 -0400

    changes to index calculations and initial outputs

--- R_code_files/08_index_calculations.R
+++ R_code_files/08_index_calculations.R
@@ -1,6 +1,6 @@
 #main index code
 
-#Load Data
+#Load Data####
 #2014
 raw_data_2014 <- read_csv(paste(final_data,"final_index_data_2014.csv",sep=""))
 #2015
@@ -17,7 +17,7 @@ raw_data_2019 <- read_csv(paste(final_data,"final_index_data_2019.csv",sep=""))
 raw_data_2020 <- read_csv(paste(final_data,"final_index_data_2020.csv",sep=""))
 
 
-#Combined Data
+#Combined Data####
 raw_data<-rbind(raw_data_2014,raw_data_2015,raw_data_2016,raw_data_2017,raw_data_2018,raw_data_2019,raw_data_2020)
 
 
@@ -67,12 +67,11 @@ alternate_scores<-data.frame(country=raw_data$country,
 zscores<-zscores[-3]
 alternate_scores<-alternate_scores[-3]
 
-
+#Flipping zeros and ones####
 
 #Multiply variables that need to be flipped by -1 (There is likely a better way to do this)
 #List of variables flipped for reference:
 # corporate_rate
-# patent_box
 # r_and_d_credit
 # corporate_time
 # profit_payments
@@ -87,14 +86,7 @@ alternate_scores<-alternate_scores[-3]
 # vat_rate
 # vat_threshold
 # consumption_time
-# property_tax
 # property_tax_collections
-# net_wealth
-# estate_or_inheritance_tax
-# transfer_tax
-# asset_tax
-# capital_duties
-# financial_transaction_tax
 # country_limitations
 # dividends_withholding_tax
 # interest_withholding_tax
@@ -102,9 +94,8 @@ alternate_scores<-alternate_scores[-3]
 # cfc_rules
 # thin_capitalization_rules
 
-flip<-c("corporate_rate","patent_box","r_and_d_credit","corporate_time","profit_payments","other_payments","vat_rate","vat_threshold","consumption_time",
-        "property_tax","property_tax_collections","net_wealth","estate_or_inheritance_tax","transfer_tax","asset_tax","capital_duties",
-        "financial_transaction_tax","capital_gains_rate","dividends_rate","top_income_rate","threshold_top_income_rate","tax_wedge","labor_payments","labor_time",
+flip<-c("corporate_rate","r_and_d_credit","corporate_time","profit_payments","other_payments","vat_rate","vat_threshold","consumption_time",
+        "capital_gains_rate","dividends_rate","top_income_rate","threshold_top_income_rate","tax_wedge","labor_payments","labor_time",
         "dividends_withholding_tax","interest_withholding_tax","royalties_withholding_tax","cfc_rules","country_limitations","thin_capitalization_rules")
 
 
@@ -126,7 +117,7 @@ for (i in flip) {
 }
 
 
-#Create Subcategories
+#Create Subcategories####
 
 #Categories and variables for Reference:
   #Corporate Rate
@@ -138,6 +129,7 @@ for (i in flip) {
     # buildings_cost_recovery
     # intangibles_cost_recovery
     # inventory
+    # allowance_corporate_equity
   #Incentives/Complexity
     # patent_box
     # r_and_d_credit
@@ -180,13 +172,15 @@ for (i in flip) {
     # dividends_withholding_taxing
     # interest_withholding_taxing
     # royalties_withholding_taxing
+  #tax treaties
     # treaties
   #regulations
     # cfc_rules
     # thin_capitalization_rules
 
 corporate_rate_index<-c("corporate_rate")
-cost_recovery_index<-c("loss_carryback","loss_carryforward","machines_cost_recovery","buildings_cost_recovery","intangibles_cost_recovery","inventory")
+cost_recovery_index<-c("loss_carryback","loss_carryforward","machines_cost_recovery",
+                       "buildings_cost_recovery","intangibles_cost_recovery","inventory","allowance_corporate_equity")
 incentives_index<-c("patent_box","r_and_d_credit","corporate_time","profit_payments","other_payments")
 consumption_tax_rate_index<-c("vat_rate")
 consumption_tax_base_index<-c("vat_threshold","vat_base")
@@ -198,7 +192,8 @@ capital_gains_and_dividends_index<-c("capital_gains_rate","dividends_rate")
 income_tax_index<-c("top_income_rate","threshold_top_income_rate","tax_wedge")
 income_tax_complexity_index<-c("labor_payments","labor_time")
 territorial_index<-c("dividends_exemption","capital_gains_exemption","country_limitations")
-withholding_index<-c("dividends_withholding_tax","interest_withholding_tax","royalties_withholding_tax","tax_treaties")
+withholding_index<-c("dividends_withholding_tax","interest_withholding_tax","royalties_withholding_tax")
+tax_treaties_index<-c("tax_treaties")
 international_regulations_index<-c("cfc_rules","thin_capitalization_rules")
 
 subcategories<-data.frame(country=zscores$country,
@@ -218,6 +213,7 @@ subcategories$income_tax<-apply((zscores[income_tax_index]*(1/length(income_tax_
 subcategories$income_tax_complexity<-apply((zscores[income_tax_complexity_index]*(1/length(income_tax_complexity_index))),1,sum)
 subcategories$territorial<-apply((zscores[territorial_index]*(1/length(territorial_index))),1,sum)
 subcategories$withholding_taxes<-apply((zscores[withholding_index]*(1/length(withholding_index))),1,sum)
+subcategories$tax_treaties<-apply((zscores[tax_treaties_index]*(1/length(tax_treaties_index))),1,sum)
 subcategories$international_regulations<-apply((zscores[international_regulations_index]*(1/length(international_regulations_index))),1,sum)
 
 #alternate_ Scoring Technique
@@ -239,9 +235,10 @@ alternate_subcategories$income_tax<-apply((alternate_scores[income_tax_index]*(1
 alternate_subcategories$income_tax_complexity<-apply((alternate_scores[income_tax_complexity_index]*(1/length(income_tax_complexity_index))),1,sum)
 alternate_subcategories$territorial<-apply((alternate_scores[territorial_index]*(1/length(territorial_index))),1,sum)
 alternate_subcategories$withholding_taxes<-apply((alternate_scores[withholding_index]*(1/length(withholding_index))),1,sum)
+alternate_subcategories$tax_treaties<-apply((alternate_scores[tax_treaties_index]*(1/length(tax_treaties_index))),1,sum)
 alternate_subcategories$international_regulations<-apply((alternate_scores[international_regulations_index]*(1/length(international_regulations_index))),1,sum)
 
-#Final Categories and Final Score with Ranks
+#Final Categories and Final Score with Ranks####
 #Each category contains three subcategories
 
 #Same thing as above
@@ -262,7 +259,7 @@ categories$income<-apply((subcategories[income_index]*(1/length(income_index))),
 categories$international<-apply((subcategories[international_index]*(1/length(international_index))),1,sum)
 categories$final<-apply((categories[3:7]*(1/length(categories[3:7]))),1,sum)
 
-write.csv(subset(categories,categories$year==2019),file = paste(final_outputs,"categories_score.csv",sep=""),row.names=F)
+write.csv(subset(categories,categories$year==2020),file = paste(final_outputs,"categories_score.csv",sep=""),row.names=F)
 
 
 
@@ -311,10 +308,10 @@ rank1<-function(x){
   ranks<-rank(-x,ties.method= "min")
   return(ranks)
 }
-write.csv(subset(subcategories,subcategories$year==2019),file = paste(final_outputs,"subcategories_z_score.csv",sep=""),row.names=F)
+write.csv(subset(subcategories,subcategories$year==2020),file = paste(final_outputs,"subcategories_z_score.csv",sep=""),row.names=F)
 
 
-#Subcategory Scores
+#Subcategory Scores####
 subcategories<-data.frame(country=raw_data$country,
                            ddply(subcategories[-1],
                                  .(year),
@@ -332,7 +329,7 @@ subcategories<-data.frame(country=raw_data$country,
 #    )
 
 
-#Add Ranks
+#Add Ranks####
 
 subcategories<-ddply(subcategories, 
                      .(year),
@@ -351,6 +348,7 @@ subcategories<-ddply(subcategories,
                      income_tax_complexity_rank = rank(-income_tax_complexity,ties.method = "min"),
                      territorial_rank = rank(-territorial,ties.method = "min"),
                      withholding_taxes_rank = rank(-withholding_taxes,ties.method = "min"),
+                     tax_treaties_rank = rank(-tax_treaties,ties.method = "min"),
                      international_regulations_rank = rank(-international_regulations,ties.method = "min")
 )
 
@@ -373,10 +371,11 @@ alternate_subcategories<-ddply(alternate_subcategories,
                         income_tax_complexity_rank = rank(-income_tax_complexity,ties.method = "min"),
                         territorial_rank = rank(-territorial,ties.method = "min"),
                         withholding_taxes_rank = rank(-withholding_taxes,ties.method = "min"),
+                        tax_treaties_rank = rank(-tax_treaties,ties.method = "min"),
                         international_regulations_rank = rank(-international_regulations,ties.method = "min")
 )
 
-#Category Scores
+#Category Scores####
 
 categories<-data.frame(country=raw_data$country,
                        ddply(categories[-1],
@@ -419,6 +418,7 @@ alternate_categories<-ddply(alternate_categories,
                      final_rank = rank(-final,ties.method = "min")                      
 )
 
+#Final files####
 #Create the final two files. One for subcategory ranks, the other for final ranks. 
 
 final_categories<-data.frame(country=zscores$country,
@@ -466,31 +466,35 @@ final_2016<-final_categories[final_categories$year==2016,]
 final_2017<-final_categories[final_categories$year==2017,]
 final_2018<-final_categories[final_categories$year==2018,]
 final_2019<-final_categories[final_categories$year==2019,]
+final_2020<-final_categories[final_categories$year==2020,]
 
-#Data Check
+#Data Check####
 
 check<-raw_data[raw_data$country == "Greece",]
 
-#Checking Sensitivity
+#Checking Sensitivity####
 
 #Does the normalization technique drive the results?
+final_categories<-final_categories[order(final_categories$country,final_categories$year),]
+alternate_final_categories<-alternate_final_categories[order(alternate_final_categories$country,alternate_final_categories$year),]
 
-cor(alternate_final_categories$final[alternate_final_categories$year == 2019],final_categories$final[final_categories$year == 2019])
+cor(alternate_final_categories$final[alternate_final_categories$year == 2020],final_categories$final[final_categories$year == 2020])
+
 
 #not really. 98 percent correlation between the two
 
-#Which Category drives the results the most?
+#Which Category drives the results the most?####
 
 #normal scoring techniques:
 
-cortest1<-final_categories[final_categories$year == 2019,]
-subcortest1<-final_subcategories[final_subcategories$year == 2019,]
+cortest1<-final_categories[final_categories$year == 2020,]
+subcortest1<-final_subcategories[final_subcategories$year == 2020,]
 subcortest1<-cbind(subcortest1,cortest1[14])
 cor(cortest1[c(4,6,8,10,12,14)])
 categories_correl<-data.frame(cor(cortest1[c(4,6,8,10,12,14)]))
 write.csv(categories_correl,paste(final_outputs,"categories_correlation.csv",sep=""))
 
-subcategories_correl<-data.frame(cor(subcortest1[c(seq(4,32,2),35)]))
+subcategories_correl<-data.frame(cor(subcortest1[c(seq(4,34,2),37)]))
 write.csv(subcategories_correl,paste(final_outputs,"subcategories_correlation.csv",sep=""))
 
 
@@ -551,13 +555,14 @@ Changes <- M[,grepl("*\\.x$",names(M))] - M[,grepl("*\\.y$",names(M))]
 
 Changes<-cbind(M[,1,drop=FALSE],Changes)
 
-final_subcategories_2019<-subset(final_subcategories,year==2019)
+final_subcategories_2020<-subset(final_subcategories,year==2020)
 
 
-write.csv(raw_data,paste(final_outputs,"raw_data_2019.csv",sep=""),row.names=F)
+write.csv(raw_data,paste(final_outputs,"raw_data_2020.csv",sep=""),row.names=F)
 write.csv(final_2017, file = paste(final_outputs,"data_2017_run.csv",sep=""),row.names=F)
 write.csv(final_2018, file = paste(final_outputs,"data_2018_run.csv",sep=""),row.names=F)
 write.csv(final_2019, file = paste(final_outputs,"data_2019_run.csv",sep=""),row.names=F)
+write.csv(final_2020, file = paste(final_outputs,"data_2020_run.csv",sep=""),row.names=F)
 
-write.csv(final_subcategories_2019,paste(final_outputs,"subcategories_2019.csv",sep=""),row.names=F)
-write.csv(final_categories,paste(final_outputs,"final_categories_2014_2019.csv",sep=""),row.names=F)
+write.csv(final_subcategories_2020,paste(final_outputs,"subcategories_2020.csv",sep=""),row.names=F)
+write.csv(final_categories,paste(final_outputs,"final_categories_2014_2020.csv",sep=""),row.names=F)
