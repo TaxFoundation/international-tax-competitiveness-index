c2cccc902f62a448bb42ae8e5810d285023ce3ac
Elke Asen <easen@taxfoundation.org>
2021-06-11 20:38:44 -0400

    Update 2021 VAT Rates
    Added 2021 VAT rates for all OECD countries and VAT rates for all years for Colombia.

--- R_code_files/03_vat_data.R
+++ R_code_files/03_vat_data.R
@@ -3,15 +3,15 @@
 #VAT Rates####
 
 vat_rates <- read_excel(paste(source_data,"oecd_vat_gst_rates_ctt_trends.xlsx",sep=""), 
-                                       range = "A4:T39")
+                                       range = "A2:Q38")
 
-vat_rates<-vat_rates[-c(3:13)]
+vat_rates<-vat_rates[-c(2:9)]
 
-colnames(vat_rates) <- c("country", "year", "2014", "2015", "2016", "2017", "2018", "2019", "2020")
+colnames(vat_rates) <- c("country", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021")
 
 #US VAT rate equivalent
 columns <- names(vat_rates)
-values <- c("United States","","7.2","7.3","7.3","7.4","7.4","7.4","7.4")
+values <- c("United States","7.2","7.3","7.3","7.4","7.4","7.4","7.4","7.4")
 US <- data.frame(columns, values)
 US <- spread(US, columns, values)
 
@@ -19,14 +19,13 @@ vat_rates <- rbind(vat_rates, US)
 
 #Canada VAT rate equivalent - https://www.retailcouncil.org/resources/quick-facts/sales-tax-rates-by-province/
 columns <- names(vat_rates)
-values <- c("Canada","","15.6","10.6","10.6","12.4","12.4","12.4","12.4")
+values <- c("Canada","15.6","10.6","10.6","12.4","12.4","12.4","12.4","12.4")
 Canada <- data.frame(columns, values)
 Canada <- spread(Canada, columns, values)
 
 vat_rates <- subset(vat_rates, vat_rates$country!="Canada*")
 vat_rates <- rbind(vat_rates, Canada)
 
-vat_rates <- vat_rates[-c(2)]
 vat_rates <- melt(vat_rates,id.vars=c("country"))
 colnames(vat_rates) <- c("country", "year", "vat_rate")
 vat_rates$country <- str_remove_all(vat_rates$country, "[*]")
