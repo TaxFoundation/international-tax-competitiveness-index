b3f34b40558fc91b660facb6ca6cb0880e2d0d1a
Elke Asen <easen@taxfoundation.org>
2021-06-11 21:16:18 -0400

    Update 2021 VAT Thresholds
    Added 2021 VAT thresholds for all OECD countries and the VAT threshold for Colombia for all years.

--- R_code_files/03_vat_data.R
+++ R_code_files/03_vat_data.R
@@ -37,7 +37,8 @@ write.csv(vat_rates,paste(intermediate_outputs,"vat_rates.csv",sep=""),row.names
 vat_thresholds_2014 <- read_excel(paste(source_data,"oecd_vat_gst_annual_turnover_concessions_ctt_trends.xlsx",sep=""), sheet = "2014", range = "a4:d37")
 vat_thresholds_2016 <- read_excel(paste(source_data,"oecd_vat_gst_annual_turnover_concessions_ctt_trends.xlsx",sep=""), sheet = "2016", range = "a4:e41")
 vat_thresholds_2018 <- read_excel(paste(source_data,"oecd_vat_gst_annual_turnover_concessions_ctt_trends.xlsx",sep=""), sheet = "2018", range = "A4:e42")
-vat_thresholds_2020 <- read_excel(paste(source_data,"oecd_vat_gst_annual_turnover_concessions_ctt_trends.xlsx",sep=""), sheet = "2020", range = "A4:e42")
+vat_thresholds_2020 <- read_excel(paste(source_data,"oecd_vat_gst_annual_turnover_concessions_ctt_trends.xlsx",sep=""), sheet = "2020", range = "A4:e43")
+vat_thresholds_2021 <- read_excel(paste(source_data,"oecd_vat_gst_annual_turnover_concessions_ctt_trends.xlsx",sep=""), sheet = "2021", range = "A4:e43")
 
 #2014
 vat_thresholds_2014 <- vat_thresholds_2014[-c(2:3)]
@@ -75,17 +76,23 @@ colnames(vat_thresholds_2020) <- c("country", "vat_threshold")
 vat_thresholds_2020$country <- str_remove_all(vat_thresholds_2020$country, "[6*]")
 vat_thresholds_2020$year <- "2020"
 
+#2021
+vat_thresholds_2021 <- vat_thresholds_2021[-c(2:4)]
+colnames(vat_thresholds_2021) <- c("country", "vat_threshold")
+vat_thresholds_2021$country <- str_remove_all(vat_thresholds_2021$country, "[6*]")
+vat_thresholds_2021$year <- "2021"
+
 #Combine years
-vat_thresholds <- rbind(vat_thresholds_2014, vat_thresholds_2015, vat_thresholds_2016, vat_thresholds_2017, vat_thresholds_2018, vat_thresholds_2019, vat_thresholds_2020)
+vat_thresholds <- rbind(vat_thresholds_2014, vat_thresholds_2015, vat_thresholds_2016, vat_thresholds_2017, vat_thresholds_2018, vat_thresholds_2019, vat_thresholds_2020, vat_thresholds_2021)
 
 #Change NAs to zeros and delete empty rows
 vat_thresholds[is.na(vat_thresholds)] <- 0
 vat_thresholds <- subset(vat_thresholds, vat_thresholds$country!="0")
 
-#Add US for all years; Latvia for 2014 and 2015; Lithuania for 2014, 2015, 2016, 2017#
-country <- c("United States","United States","United States","United States","United States","United States", "United States")
-vat_threshold <- c("0","0","0","0","0","0","0")
-year <- c("2014","2015","2016","2017","2018","2019","2020")
+#Add US for all years; Latvia for 2014 and 2015; Lithuania for 2014, 2015, 2016, 2017; and Colombia for 2014-2019#
+country <- c("United States","United States","United States","United States","United States","United States", "United States", "United States")
+vat_threshold <- c("0","0","0","0","0","0","0", "0")
+year <- c("2014","2015","2016","2017","2018","2019","2020", "2021")
 USA <- data.frame(country, vat_threshold, year)
 
 country <- c("Latvia", "Latvia")
@@ -98,7 +105,12 @@ vat_threshold <- c("101580", "100897", "100671", "100223")
 year <- c("2014", "2015", "2016", "2017")
 LTU <- data.frame(country,vat_threshold,year)
 
-vat_thresholds <- rbind(vat_thresholds, USA, LVA, LTU)
+country <- c("Colombia","Colombia","Colombia","Colombia","Colombia","Colombia")
+vat_threshold <- c("0","0","0","0","0","0")
+year <- c("2014","2015","2016","2017","2018","2019")
+COL <- data.frame(country, vat_threshold, year)
+
+vat_thresholds <- rbind(vat_thresholds, USA, LVA, LTU, COL)
 
 write.csv(vat_thresholds,paste(intermediate_outputs,"vat_thresholds.csv",sep=""),row.names = FALSE)
 
