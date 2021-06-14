ce32adde1a8a74b9c4ca6d14acda2687c7ecbfa3
Daniel Bunn <dbunn@taxfoundation.org>
2021-06-14 13:56:19 -0400

    cfc_rules_data intermediate_outputs update

--- R_code_files/05_cfc_rules.R
+++ R_code_files/05_cfc_rules.R
@@ -6,8 +6,9 @@ cfc_rules_2017 <- read_csv(paste(source_data,"cfc_rules_2017.csv",sep=""))
 cfc_rules_2018 <- read_csv(paste(source_data,"cfc_rules_2018.csv",sep=""))
 cfc_rules_2019 <- read_csv(paste(source_data,"cfc_rules_2019.csv",sep=""))
 cfc_rules_2020 <- read_csv(paste(source_data,"cfc_rules_2020.csv",sep=""))
+cfc_rules_2021 <- read_csv(paste(source_data,"cfc_rules_2021.csv",sep=""))
 
-cfc_rules<-rbind(cfc_rules_2014,cfc_rules_2015,cfc_rules_2016,cfc_rules_2017,cfc_rules_2018,cfc_rules_2019,cfc_rules_2020)
+cfc_rules<-rbind(cfc_rules_2014,cfc_rules_2015,cfc_rules_2016,cfc_rules_2017,cfc_rules_2018,cfc_rules_2019,cfc_rules_2020,cfc_rules_2021)
 
 cfc_rules$cfc_rules<-rowMeans(cfc_rules[,5:7])
 
