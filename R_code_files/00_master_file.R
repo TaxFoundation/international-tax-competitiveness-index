5374dbf00255c29d52f7a8e17151259ec6b63f59
Daniel Bunn <dbunn@taxfoundation.org>
2022-10-21 14:10:09 -0400

    Baseline and scenario run
    - full expensing for equipment and machinery
    -corporate rate to OECD average

--- R_code_files/00_master_file.R
+++ R_code_files/00_master_file.R
@@ -10,6 +10,7 @@ intermediate_outputs<-"C:/Github/international-tax-competitiveness-index/interme
 final_data<-"C:/Github/international-tax-competitiveness-index/final_data/"
 final_outputs<-"C:/Github/international-tax-competitiveness-index/final_outputs/"
 country_outputs<-"C:/Github/international-tax-competitiveness-index/country_outputs/"
+germany_reforms<-"C:/Github/international-tax-competitiveness-index/germany_reforms/"
 
 
 #Define Using function####
