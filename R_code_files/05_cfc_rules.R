b352faf67afd1d96b8e9a8e12c65739871303057
Alex Mengden <amengden@taxfoundation.org>
2023-06-15 16:05:14 +0200

    CFC updates (non-final)
    R-code and source data; further review of country exemptions required

--- R_code_files/05_cfc_rules.R
+++ R_code_files/05_cfc_rules.R
@@ -8,10 +8,13 @@ cfc_rules_2019 <- read_csv(paste(source_data,"cfc_rules_2019.csv",sep=""))
 cfc_rules_2020 <- read_csv(paste(source_data,"cfc_rules_2020.csv",sep=""))
 cfc_rules_2021 <- read_csv(paste(source_data,"cfc_rules_2021.csv",sep=""))
 cfc_rules_2022 <- read_csv(paste(source_data,"cfc_rules_2022.csv",sep=""))
+cfc_rules_2023 <- read_csv(paste(source_data,"cfc_rules_2022.csv",sep=""))
+
 
 cfc_rules<-rbind(cfc_rules_2014,cfc_rules_2015,cfc_rules_2016,
                  cfc_rules_2017,cfc_rules_2018,cfc_rules_2019,
-                 cfc_rules_2020,cfc_rules_2021,cfc_rules_2022)
+                 cfc_rules_2020,cfc_rules_2021,cfc_rules_2022,
+                 cfc_rules_2023)
 
 cfc_rules$cfc_rules<-rowMeans(cfc_rules[,5:8])
 
